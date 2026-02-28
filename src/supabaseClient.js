c22a7a6e5456c9da571ac89ed56ae648d3970c92
Aaditya Pratap <aadityapratapiit@gmail.com>
2026-02-28 13:30:32 +0530

    made changes for supabase dns issue caused by jio dns blocking of supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
+let supabaseUrl =
   import.meta.env.VITE_SUPABASE_URL ||
   "https://xxxxxxxxxxxxxxxxxxxx.supabase.co";
+
+// If using proxy on Vercel, dynamically construct full URL if using a relative path like '/supabase'
+if (supabaseUrl.startsWith("/")) {
+  supabaseUrl = window.location.origin + supabaseUrl;
+}
+
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "public-anon-key";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
